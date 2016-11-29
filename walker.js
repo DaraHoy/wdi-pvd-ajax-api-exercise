f0027c2c7971b0eabcda3782709077ec5844fc68
yourUsername <your_email@example.com>
2016-11-29 06:48:47 -0500

    hw

---
+++ walker.js
@@ -0,0 +1,51 @@
+// Get the walker image:
+var walker = document.getElementById('walker');
+
+// Configure motion params:
+var walkingLeft = false;
+var leftBorder = 0;
+var rightBorder = window.innerWidth - walker.offsetWidth;
+var speed = 10;
+var xPos = leftBorder;
+
+// The update function is called once every "frame" (via setInterval)
+// Changing the walker's position each frame, will cause him to move across the screen
+function update() {
+  // ensure walker is facing to the right (reverse of "normal")
+  // CSS does this for us, we just assign the right class.
+  walker.classList.add("flip");
+
+  // Move "speed" pixels per iteration:
+  xPos += speed;
+  // Reset back to left side, when we run into right wall
+  if(xPos >= rightBorder){
+    xPos = leftBorder;
+  }
+  // reposition the walker
+  walker.style.left = xPos + "px";
+}
+
+// Establish an update interval (framerate)
+// This will call the "update" method every 100ms
+// Add code to "update" to change the walker's position
+setInterval(update, 100);
+
+// Change right border when resized
+window.addEventListener("resize", function(){
+  rightBorder = window.innerWidth - walker.offsetWidth;
+});
+
+let url = "http://api.icndb.com/jokes/random";
+$('#walker').on('click',function(){
+  //$('#quote').empty();
+  $.ajax({
+    url: "http://api.icndb.com/jokes/random",
+    type: 'GET',
+    dataType: 'json'
+  }).done(function(response){
+    console.log(response);
+    $('#quote').html(response);
+  }).fail(function(){
+    console.log("fail");
+  });
+});
